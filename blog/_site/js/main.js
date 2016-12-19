82e69d05273d7b9c82cbe3e1c7b073cdda16cbb8
thaihust <thaiph.hust@gmail.com>
2016-12-19 14:15:56 +0700

    reinstall jekyll

--- blog/_site/js/main.js
+++ blog/_site/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
-  if (window.location.pathname !== '' && window.location.pathname !== 'index.html') {
+  if (window.location.pathname !== '/blog/' && window.location.pathname !== '/blog/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
